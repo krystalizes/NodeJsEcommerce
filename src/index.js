979559bba942a8230805355cf026305f24ca2942
krystalizes <91723136+krystalizes@users.noreply.github.com>
2024-04-12 15:58:05 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const app = express();
+const port = 3000;
+// const SortMiddleware = require('./app/middleware/SortMiddleware');
+// const route = require('./routes');
+// const db = require('./config/db');
+
+// db.connected();
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(
+//     express.urlencoded({
+//         extended: true,
+//     }),
+// );
+// app.use(express.json());
+// app.use(methodOverride('_method'));
+// app.use(SortMiddleware);
+// app.use(morgan('combined'))
+
+// app.engine(
+//     'hbs',
+//     exphbs.engine({
+//         extname: '.hbs',
+//         helpers: {
+//             sum: (a, b) => a + b,
+//             sortable: (field, sort) => {
+//                 const sortType = field === sort.column ? sort.type : 'default';
+//                 const icons = {
+//                     default: 'code',
+//                     asc: 'arrow-up',
+//                     desc: 'arrow-down',
+//                 };
+//                 const types = {
+//                     default: 'desc',
+//                     asc: 'desc',
+//                     desc: 'asc',
+//                 };
+//                 const icon = icons[sortType];
+//                 const type = types[sortType];
+//                 return `<a href="?_sort&column=${field}&type=${type}">
+//                 <i class="sort ${icon === 'code' ? ' rotate' : ''}" data-feather="${icon}"></i>
+//               </a>`;
+//             },
+//         },
+//     }),
+// );
+// app.set('view engine', 'hbs');
+// app.set('views', path.join(__dirname, 'resources', 'views'));
+
+// route(app);
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
